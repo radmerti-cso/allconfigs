9814d86085a3f62123dc50ea9e984ada03053028
Tillmann Radmer <tillmann.radmer@gmail.com>
2021-07-29 11:47:37 +0200

    Update starship-prompt.ps1

--- powershell/starship-prompt.ps1
+++ powershell/starship-prompt.ps1
@@ -1,3 +1,9 @@
+# Install "scoop" package manager
+iwr -useb get.scoop.sh | iex
+
+# install starship cross-shell prompt
+scoop install starship
+
 # Add this to the end of your "%USER%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 # If you get an error "running scripts is disabled on this system" run "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted"
 Invoke-Expression (&starship init powershell)
