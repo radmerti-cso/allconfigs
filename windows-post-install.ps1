6f9861f3770d66489277eb3b19ce31b706aa87d3
Tillmann Radmer <tillmann.radmer@gmail.com>
2021-10-13 16:40:30 +0200

    Update windows-post-install.ps1

--- windows-post-install.ps1
+++ windows-post-install.ps1
@@ -3,3 +3,17 @@ iwr -useb get.scoop.sh | iex
 
 # install starship cross-shell prompt
 scoop install starship
+
+# setup Python Development Master (PDM) in PowerShell
+# Create a directory to store completion scripts
+mkdir $PROFILE\..\Completions
+echo @'
+
+# initialize auto completions for Python Development Master (PDM)
+Get-ChildItem "$PROFILE\..\Completions\" | ForEach-Object {
+    . $_.FullName
+}
+'@ | Out-File -Append -Encoding utf8 $PROFILE
+# Generate script
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+pdm completion powershell | Out-File -Encoding utf8 $PROFILE\..\Completions\pdm_completion.ps1
